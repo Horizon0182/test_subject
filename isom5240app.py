aa7ef079013ed4d87740a9d964088a82c963b48e
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-02-14 17:20:51 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -2,3 +2,19 @@ import streamlit as st
 
 st.write("ISOM5240")
 st.write("ISOM5240")
+
+from transformers import pipeline
+
+# Load text generation pipeline
+# Specify the model you want to use
+generator = pipeline("text-generation",
+                     model="distilbert/distilgpt2")
+
+# Generate text
+prompt = "Once upon a time in a land far, far away"
+generated_story = generator(prompt,
+                            max_length=150)
+
+# Output
+print("Generated Story:")
+print(generated_story[0]['generated_text'])
