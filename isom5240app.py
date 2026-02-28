b5dd603b393ff431a41e1fcd3938a0f1b756cb68
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-02-28 17:38:29 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -2,21 +2,25 @@ from transformers import pipeline
 from PIL import Image
 import streamlit as st
 
-# Streamlit UI
-st.header("Title: Age Classification using ViT")
+def img_classify():
+  age_classifier = pipeline("image-classification", model="ibombonato/swin-age-classifier")
+  image_name = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+  image_name = Image.open(image_name).convert("RGB")
+  # Classify age
+  age_predictions = age_classifier(image_name)
+  st.write(age_predictions)
+  age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
 
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification", model="ibombonato/swin-age-classifier")
+  return age_predictions
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
 
-# Classify age
-age_predictions = age_classifier(image_name)
-st.write(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+def main():
+  st.header("Age classifier")
 
-# Display results
-st.write("Predicted Age Range:")
-st.write(f"Age range: {age_predictions[0]['label']}")
+  img_classify()
+
+  st.write("Predicted Age Range:")
+  st.write(f"Age range: {age_predictions[0]['label']}")
+
+if __name__ == "__main__":
+    main()
