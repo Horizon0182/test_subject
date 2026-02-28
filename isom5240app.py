c818837d4ea423e28f1a752431c189a5cd0af1de
Horizon0182 <148845763+Horizon0182@users.noreply.github.com>
2026-02-28 15:42:13 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,51 +1,66 @@
-import streamlit as st
+# Program title: Storytelling App
 
-st.write("ISOM5240")
-st.write("ISOM5240")
-
+# import part
 import streamlit as st
 from transformers import pipeline
 
-# Load the text classification model pipeline
-classifier = pipeline("text-classification",model='isom5240ust/bert-base-uncased-emotion')
+# function part
+# img2text
+def img2text(url):
+    image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
+    text = image_to_text_model(url)[0]["generated_text"]
+    return text
 
-# Streamlit application title
-st.title("Text Classification for you")
-st.write("Classification for 6 emotions: sadness, joy, love, anger, fear, surprise")
+# text2story
+def text2story(text):
+    pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
+    story_text = pipe(text)[0]['generated_text']
+    return story_text
 
-# Text input for user to enter the text to classify
-text = st.text_area("Enter the text to classify", "")
+# text2audio
+def text2audio(story_text):
+    pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
+    audio_data = pipe(story_text)
+    return audio_data
 
-# Perform text classification when the user clicks the "Classify" button
-if st.button("Classify"):
-    # Perform text classification on the input text
-    result = classifier(text)[0]
 
-    # Display the classification result
-    st.write("Text:", text)
-    st.write("Label:", result['score'])
-    st.write("Score:", result['label'])
+def main():
+    st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
+    st.header("Turn Your Image to Audio Story")
+    uploaded_file = st.file_uploader("Select an Image...")
 
-from PIL import Image
-import time
+    if uploaded_file is not None:
+        print(uploaded_file)
+        bytes_data = uploaded_file.getvalue()
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-# App title
-st.title("Streamlit Demo on Hugging Face")
 
-# Write some text
-st.write("Welcome to a demo app showcasing basic Streamlit components!")
+        #Stage 1: Image to Text
+        st.text('Processing img2text...')
+        scenario = img2text(uploaded_file.name)
+        st.write(scenario)
 
-# File uploader for image and audio
-uploaded_image = st.file_uploader("Upload an image",
-                                  type=["jpg", "jpeg", "png"])
+        #Stage 2: Text to Story
+        st.text('Generating a story...')
+        story = text2story(scenario)
+        st.write(story)
 
-# Display image with spinner
-if uploaded_image is not None:
-    with st.spinner("Loading image..."):
-        time.sleep(1)  # Simulate a delay
-        image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        #Stage 3: Story to Audio data
+        st.text('Generating audio data...')
+        audio_data =text2audio(story)
 
-# Button interaction
-if st.button("Click Me"):
-    st.write("🎉 You clicked the button!")
+        # Play button
+        if st.button("Play Audio"):
+            # Get the audio array and sample rate
+            audio_array = audio_data["audio"]
+            sample_rate = audio_data["sampling_rate"]
+
+            # Play audio directly using Streamlit
+            st.audio(audio_array,
+                     sample_rate=sample_rate)
+
+
+if __name__ == "__main__":
+    main()
